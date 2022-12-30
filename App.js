b9faedb659c16f07e09952e27aedfa0a0add12c4
Hye In Shin <hyein0608sa@gmail.com>
2022-12-30 13:01:11 +0900

    init

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+
+export default function App(){
+  //return (<MainPage/>)
+  return (<AboutPage/>)
+}
