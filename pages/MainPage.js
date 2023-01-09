ecc8876142d6455062f1fb03d82568f62b5f2a7b
Hye In Shin <hyein0608sa@gmail.com>
2023-01-10 01:00:31 +0900

    main page status bar

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -5,6 +5,7 @@ const main = 'https://storage.googleapis.com/sparta-image.appspot.com/lecture/ma
 import data from '../data.json';
 import Card from '../components/Card';
 import Loading from '../components/Loading';
+import { StatusBar } from 'expo-status-bar';
 export default function MainPage() {
   //useState 사용법
 	//[state,setState] 에서 state는 이 컴포넌트에서 관리될 상태 데이터를 담고 있는 변수
@@ -53,6 +54,7 @@ export default function MainPage() {
       return 구문 안에서는 {슬래시 + * 방식으로 주석
     */
     <ScrollView style={styles.container}>
+      <StatusBar style="light" />
       <Text style={styles.title}>나만의 꿀팁</Text>
 			 <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
       <Image style={styles.mainImage} source={{uri:main}}/>
